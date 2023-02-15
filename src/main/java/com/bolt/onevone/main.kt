dd9dea55be7393b786af3d90e9e4d2d7b85ac52b
Anonymous Dev <125176344+ItzPhoenixYaKnow@users.noreply.github.com>
2023-02-15 00:28:39 -0800

    Update main.kt

--- src/main/java/com/bolt/onevone/main.kt
+++ src/main/java/com/bolt/onevone/main.kt
@@ -1,14 +1,44 @@
-package com.bolt.onevone;
+package com.bolt.onevone
 
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import org.bukkit.event.player.PlayerChatEvent
 
-class OneVOne : JavaPlugin() {
+class oenvone : JavaPlugin(), CommandExecutor, Listener {
 
     override fun onEnable() {
-        logger.info("1v1 has been enabled!")
+        // Registers the event listener
+        server.pluginManager.registerEvents(MyListener(), this)
+        server.pluginManager.registerEvents(this, this)
+        getCommand("1v1").executor = this
+        }
     }
 
-    override fun onDisable() {
-        logger.info("1v1 has been disabled!")
+override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (sender is Player) {
+            val message = args.joinToString(separator = " ")
+            val event = PlayerChatEvent(sender, message)
+            server.pluginManager.callEvent(event)
+        }
+        return true
+    }
+
+    @EventHandler
+    fun onPlayerChat(event: PlayerChatEvent) {
+        event.player.sendMessage("&c[TEST] &eYou said: &c${event.message}")
+    }
+
+    private class MyListener : Listener {
+        @EventHandler
+        fun onPlayerJoin(event: PlayerJoinEvent) {
+            val player = event.player
+            player.sendMessage("&l&eWelcome to the server, &l&b${player.name}!")
+        }
     }
 }
