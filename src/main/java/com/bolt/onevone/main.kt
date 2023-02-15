5184812f1592f46097bae8ce227eb600ac86c66b
Anonymous Dev <125176344+ItzPhoenixYaKnow@users.noreply.github.com>
2023-02-15 00:19:40 -0800

    Create main.kt

---
+++ src/main/java/com/bolt/onevone/main.kt
@@ -0,0 +1,14 @@
+package com.bolt.oenvone;
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class OneVOne : JavaPlugin() {
+
+    override fun onEnable() {
+        logger.info("1v1 has been enabled!")
+    }
+
+    override fun onDisable() {
+        logger.info("1v1 has been disabled!")
+    }
+}
